1347e77dd109f00780ca1f2225fcef675ef43b77
Ebuka Oraezu <ebukaoraezu44@gmail.com>
2026-06-30 21:19:39 +0100

    first commit

---
+++ src/components/Services.js
@@ -0,0 +1,28 @@
+import Title from "./Title";
+import { service } from "../data";
+
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title='our' subtitle='services' />
+      <div className="section-center services-center">
+        {service.map((serv) => {
+          const{id, icon, title, text} = serv
+          return (
+            <article className="service" key={id}>
+              <span className="service-icon">
+                <i className={icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{title}</h4>
+                <p className="service-text">{text}</p>
+              </div>
+            </article>
+          );
+        })}
+      
+      </div>
+    </section>
+  );
+}
+export default Services
